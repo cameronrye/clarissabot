ea3fa128ec98b5fed4c9a12d49f071bfef3ff6a6
Cameron Rye <c@meron.io>
2025-12-19 08:07:59 -0500

    fix: pass API_KEY header from nginx proxy to backend API

--- src/ClarissaBot.Web/docker-entrypoint.sh
+++ src/ClarissaBot.Web/docker-entrypoint.sh
@@ -9,6 +9,14 @@ else
     sed -i "s|API_URL_PLACEHOLDER|http://clarissabot-api-dev|g" /etc/nginx/nginx.conf
 fi
 
+# Substitute API_KEY environment variable in nginx config
+if [ -n "$API_KEY" ]; then
+    sed -i "s|API_KEY_PLACEHOLDER|$API_KEY|g" /etc/nginx/nginx.conf
+else
+    # Remove the API key header line if not set
+    sed -i "s|proxy_set_header X-API-Key API_KEY_PLACEHOLDER;||g" /etc/nginx/nginx.conf
+fi
+
 # Execute the main command
 exec "$@"
 
